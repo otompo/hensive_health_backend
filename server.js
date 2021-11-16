e1111dc72efcf9c5acd1af9c310f00998c38ce10
Issah <abagnasalifusasco@gmail.com>
2021-11-16 13:38:49 +0000

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 // db
 
 mongoose
-  .connect(process.env.DATABASE_LOCAL, {
+  .connect(process.env.MONGO_URI, {
     // useNewUrlParser: true,
     // usefindandmodify: false,
     // useUnifiedTopology: true,
